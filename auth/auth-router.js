f7758b0655c0d06352147f57b0e7641b8a5c89a8
Jnmendza <jonatmendoza@gmail.com>
2019-11-15 08:37:40 -0800

    user model built

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const restricted = require('./user-model.js')
+const Users = require('./user-model.js')
+
+
 
 router.post('/register', (req, res) => {
   // implement registration
+
+    
 });
 
 router.post('/login', (req, res) => {
